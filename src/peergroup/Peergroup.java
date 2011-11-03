139857bbd5f3f5e7ad3e414b29758787ad27c6c3
Nicolas Inden <nicolas.inden@rwth-aachen.de>
2011-11-03 15:37:07 +0100

    Converted to NetBeans-Project

---
+++ src/peergroup/Peergroup.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peergroup;
+
+/**
+ *
+ * @author Nicolas Inden
+ */
+public class Peergroup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Logger log = new Logger();
+        log.addMsg("Starting Peergroup...",2);
+        log.closeLog();
+    }
+}
